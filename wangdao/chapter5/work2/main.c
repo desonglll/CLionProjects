fade469e09e575ca4db89699e4a7f5bd0595aca0
desonglll <lindesong666@163.com>
2022-12-24 13:32:16 +0800

    update work2 of chapter5

--- wangdao/chapter5/work2/main.c
+++ wangdao/chapter5/work2/main.c
@@ -2,7 +2,22 @@
 #include <string.h>
 
 int main() {
-
-
+    char a[20];
+    gets(a);
+    char b[20];
+    for (int i = strlen(a) - 1, j = 0; i >= 0, j < strlen(a); i--, j++) {
+        b[j] = a[i];
+    }
+    b[strlen(a)] = '\0';
+    int result = strcmp(a, b);
+    if (result < 0) {
+        printf("%d\n", -1);
+    } else if (result > 0) {
+        printf("%d\n", 1);
+    } else {
+        printf("%d\n", 0);
+    }
+//    puts(a);
+//    puts(b);
     return 0;
 }
