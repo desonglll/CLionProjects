a95d19035e656bac8c8b241da559940ec661bcfe
desonglll <lindesong666@163.com>
2023-01-07 10:26:05 +0800

    update chapter 7 work 1

--- wangdao/chapter6/work2/main.c
+++ wangdao/chapter6/work2/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main() {
-    int size;
-    scanf("%d\n", &size);
-    char *p = NULL;
-    p = (char *) malloc(size);
-//    char c;
-//    scanf("%c", &c);
-    gets(p);
-    puts(p);
-    free(p);
-    return 0;
+  int size;
+  scanf("%d\n", &size);
+  char *p = NULL;
+  p = (char *) malloc(size);
+  //    char c;
+  //    scanf("%c", &c);
+  gets(p);
+  puts(p);
+  free(p);
+  return 0;
 }
--- wangdao/chapter7/work1/main.c
+++ wangdao/chapter7/work1/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+int step(int);
+
 int main() {
-    printf("Hello, World!\n");
+    int n;
+    scanf("%d", &n);
+    printf("step(%d) = %d\n", n, step(n));
     return 0;
+}
+
+int step(int n) {
+    if (1 == n || 2 == n) {
+        return n;
+    }
+    return step(n - 1) + step(n - 2);
 }
